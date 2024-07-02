bc4cac6f65e2575aa194a60ab48ad24a85d0920d
KaiKim <kai.kim@groundx.xyz>
2024-07-02 14:32:22 +0900

    initial set-up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: "export",
+  basePath: "/kai",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
